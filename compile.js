9fcd3675fa1b79320b479be11ae5d149f0dddb4e
LuisEnMarroquin <mluis651@gmail.com>
2020-09-18 23:33:35 -0500

    Simplified deployment and compilation

--- compile.js
+++ compile.js
@@ -1,18 +1,26 @@
+const { readdir, writeFileSync } = require('fs')
+const { exec } = require('child_process')
+const { compileFile } = require('pug')
 const { join } = require('path')
-const pug = require('pug')
-const fs = require('fs')
 
 const viewsDirectory = join(__dirname, 'views')
 const publicDirectory = join(__dirname, 'public')
 
-fs.readdir(viewsDirectory, (err, files) => {
+readdir(viewsDirectory, (error, files) => {
+  if (error) return
   files.forEach(file => {
     var array = file.split('.')
     if (array.length !== 2) return
     if (array[1] !== 'pug') return
     var name = array[0]
-    const compiledFunction = pug.compileFile(`views/${name}.pug`)
+    const compiledFunction = compileFile(`views/${name}.pug`)
     const compiledHTML = compiledFunction({ environment: 'Production' })
-    fs.writeFileSync(join(publicDirectory, `${name}.html`), compiledHTML)
-  });
-});
+    writeFileSync(join(publicDirectory, `${name}.html`), compiledHTML)
+  })
+})
+
+exec('npm run stylus', (error, stdout, stderr) => {
+  if (error) return console.error(`error: ${error.message}`)
+  if (stderr) return console.error(`stderr: ${stderr}`)
+  console.log(`stdout:\n${stdout}`)
+})
